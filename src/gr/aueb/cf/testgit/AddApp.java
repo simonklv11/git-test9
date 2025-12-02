2cb1c209548c14e7bef41b2006307dec0893a9b0
simonklv11 <simonklv11@gmail.com>
2025-12-02 19:46:46 +0200

    feat: add multiply app

--- src/gr/aueb/cf/testgit/AddApp.java
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -1,4 +1,12 @@
 package gr.aueb.cf.testgit;
 
 public class AddApp {
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
 }
