bf137dba631c22170f1856e95a3bb9f3307a0403
simonklv11 <simonklv11@gmail.com>
2025-12-02 20:50:22 +0200

    feat: new print and mod app

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.testgit;
+
+public class ModApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/gr/aueb/cf/testgit/NewHelloApp.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.testgit;
+
+public class NewHelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World again!");
+    }
+}
